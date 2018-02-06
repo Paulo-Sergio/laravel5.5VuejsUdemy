f54b4714debe7595c9c1a7522f248c5f40e76345
Paulo-Sergio <paulofranca.contato@gmail.com>
2018-02-05 23:09:56 -0200

    criando Controllers/Admin/ArtigosController.php e routes de Atigos

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -19,3 +19,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::middleware(['auth'])->prefix('admin')->namespace('Admin')->group(function() {
+    Route::resource('/artigos', 'ArtigosController');
+});
